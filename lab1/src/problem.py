17bf49f37de67db54c5d490b9a816c6ddb10fbe0
Michal Luszczek <michluszcz@gmail.com>
2023-10-12 13:55:46 +0200

    added function and gradient value methods

--- lab1/src/problem.py
+++ lab1/src/problem.py
@@ -1,11 +1,15 @@
 
 
 class Problem:
-    def __init__(self, function: function, gradient: list):
+    def __init__(self, function: function, gradient: tuple):
         self.function = function
         self.gradient = gradient
 
-    def value(self, x0: tuple):
+    def function_value(self, x0: tuple):
         return self.function(x0)
+
+    def gradient_value(self, x0: tuple) -> tuple:
+        return self.gradient(x0)
+
 
 
