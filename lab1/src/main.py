fc2cf76778ee484599788556f1aeb1b7ffef9ec8
Michał Łuszczek <michluszcz@gmail.com>
2023-10-18 21:39:05 +0200

    created main function for testing

---
+++ lab1/src/main.py
@@ -0,0 +1,22 @@
+from problem import Problem
+from gradient_solver import GradientSolver
+from numpy import euler_gamma
+
+
+
+
+
+def main():
+    solver = GradientSolver(0.2)
+    def square_funct(x):
+        return [1/4 * (x[0] ** 4)]
+    
+    def gradient(x):
+        return [x[0] ** 3]
+    
+    problem = Problem(square_funct, gradient)
+    print(solver.solve(problem, [2]))
+    
+    
+if __name__ == "__main__":
+    main()
