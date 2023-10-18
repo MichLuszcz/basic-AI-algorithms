7300acf9d77c9dfd42a150973326f528cc0b1481
Michał Łuszczek <michluszcz@gmail.com>
2023-10-18 23:46:45 +0200

    changed the testing to funct2

--- lab1/src/main.py
+++ lab1/src/main.py
@@ -1,9 +1,10 @@
 from problem import Problem
 from gradient_solver import MinSolver
+from graph_maker import funct2, gradient2
 
 
 def main():
-    solver = MinSolver(0.2, 0.001)
+    solver = MinSolver(0.1, 0.001)
 
     def square_funct(x):
         return 1 / 4 * (x[0] ** 4)
@@ -11,8 +12,8 @@ def main():
     def gradient(x):
         return [x[0] ** 3]
 
-    problem = Problem(square_funct, gradient)
-    print(solver.solve(problem, [2]))
+    problem = Problem(funct2, gradient2)
+    print(solver.solve(problem, [1, -2.5]))
 
 
 if __name__ == "__main__":
