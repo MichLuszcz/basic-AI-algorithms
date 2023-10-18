bf55e816930924f1d080531a8ab3bdbd3372744f
Michał Łuszczek <michluszcz@gmail.com>
2023-10-18 21:39:56 +0200

    created a graph maker file with no implementation

---
+++ lab1/src/graph_maker.py
@@ -0,0 +1,15 @@
+from numpy import euler_gamma
+
+
+
+def funct1(x):
+    return [1/4 * (x[0] ** 4)]
+
+
+def gradient1(x):
+    return [x[0] ** 3]
+
+
+def funct2(x):
+    return 1.5 - euler_gamma**(-x[0]**2 - x[1]**2) - 0.5 * euler_gamma ** ( -(x[0] - 1)**2 - ((x[1] - 1)**2))
+    
