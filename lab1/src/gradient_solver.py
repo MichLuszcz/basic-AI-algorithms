67276a996b782256d5a293f4e8bf942214dfc986
Michal Luszczek <michluszcz@gmail.com>
2023-10-19 13:09:24 +0200

    added feedback and todos

--- lab1/src/gradient_solver.py
+++ lab1/src/gradient_solver.py
@@ -4,28 +4,35 @@ from numpy import linalg
 from copy import copy
 
 DefaultBeta = 0.4
+DefaultEpsilon = 0.05
+MaxIterations = 150
 
 
 class MinSolver(Solver):
-    def __init__(self, beta=DefaultBeta, epsilon=0.05) -> None:
+    def __init__(self, beta=DefaultBeta, epsilon=DefaultEpsilon) -> None:
         self.beta = beta
         self.epsilon = epsilon
 
     def get_parameters(self):
         """Returns a dictionary of hyperparameters"""
-        return {"beta": self.beta}
+        return {"beta": self.beta, "epsilon": self.epsilon}
 
     def solve(self, problem: Problem, x0: list, *args, **kwargs):
         """
         A method that solves the given problem for given initial solution.
         """
-        x = x0
+        # nazwy zmiennych
+        current_point = x0
         visited_points = []
+        # TODO zmienić na while loop
+        iterations = 0
+        # while visited_points < MaxIterations and
         for n in range(100):
             d = problem.gradient_value(x)
-            if linalg.norm(d) <= self.epsilon:
-                return x, visited_points
+            if linalg.norm(d) <= self.epsilon:  # if close enough to local minimum, stop
+                return current_point, visited_points
             visited_points.append(copy(x))
+
             for i in range(len(x)):
                 x[i] -= self.beta * d[i]
         return x, visited_points
