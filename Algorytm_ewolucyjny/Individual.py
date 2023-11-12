65346bb6fd57d457aa6ee027a6e6b34f7bb2a877
Michał Łuszczek <michluszcz@gmail.com>
2023-11-12 17:00:53 +0100

    defined Individual

---
+++ Algorytm_ewolucyjny/Individual.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+# class holding a list describing the path taken as a list of city numbers
+class Individual:
+    def __init__(self, path) -> None:
+        self.path = path
+
+    def mutate():
+        pass
