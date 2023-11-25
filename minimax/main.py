5e757cb925a7111ad51c678180d8fbdd4759f358
Michał Łuszczek <michluszcz@gmail.com>
2023-11-25 19:49:42 +0100

    Implemented game loop

---
+++ minimax/main.py
@@ -0,0 +1,20 @@
+from TicTacToe import TicTacToe
+from Player import HumanPlayer
+from minimax import MinimaxPlayer
+
+
+def main():
+    player_1 = HumanPlayer("player", "human")
+    player_2 = MinimaxPlayer("minimax", "bot")
+    game = TicTacToe(player_1, player_2)
+    while not game.game_over:
+        game.display_board()
+        game.make_move(game.get_player_move())
+        print("_" * 15)
+
+    if game.winner is not None:
+        print("Winner:" + game.winner.name)
+
+
+if __name__ == "__main__":
+    main()
