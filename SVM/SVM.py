318ee9e58a82f0a41af31139f7be1698709be276
Michał Łuszczek <michluszcz@gmail.com>
2023-12-11 00:21:22 +0100

    implemented linear svm

---
+++ SVM/SVM.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+class SVM:
+
+    def __init__(self, lambda_param, target_number):
+        self.bias = 0
+        self.weights = None
+        self.lambda_param = lambda_param
+        # the number the machine is trying to recognise
+        self.target_number = target_number
+
+    def train(self, x: np.ndarray, y: np.ndarray, learning_rate=0.01, epochs=1000):
+        # The number of Samples in X
+        number_of_samples = x.shape[0]
+
+        # The number of features in X
+        number_of_features = x.shape[1]
+        self.weights = np.zeros((1, number_of_features))
+        binary_labels = np.where(y == self.target_number, 1, -1)
+
+        for _ in range(epochs):
+            for sample_index in range(number_of_samples):
+                # our cost function for single specimen = lambda * ||w||^2 + max(0, 1 - yi * (w * xi - b))
+                # this means that its just lambda * ||w||^2 when yi * f(x) >= 1
+
+                derivative_condition = binary_labels[sample_index] * np.dot(x[sample_index], self.weights) >= 1
+
+                if derivative_condition:
+                    self.weights -= learning_rate * (2 * self.lambda_param * self.weights)
+                else:
+                    self.weights -= (learning_rate *
+                                     (2 * self.lambda_param * self.weights
+                                      - np.dot(x[sample_index], binary_labels[sample_index])))
+                    self.bias = learning_rate * binary_labels[sample_index]
+
+        return
+
+    def predict(self, x):
+        return np.sign(np.dot(x, self.weights) - self.bias)
---
+++ SVM/main.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from SVM import SVM
+import mnist
+
+czumpi_array = mnist.test_images()
+
+svm = SVM(1)
